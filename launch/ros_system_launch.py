9f8c3233b4d587dd9152978e560c3f9f3ab1ce4f
Jere Lotvonen <jere.lotvonen@gmail.com>
2025-10-16 17:44:44 +0300

    remove namespace from launch file and add parameters as dict

--- launch/ros_system_launch.py
+++ launch/ros_system_launch.py
@@ -7,26 +7,28 @@ def generate_launch_description():
         [
             Node(
                 package="hesai_ros_driver",
-                namespace="ros_control",
                 executable="hesai_ros_driver_node",
                 name="lidar_driver",
             ),
             Node(
                 package="python_lidar_subscriber",
-                namespace="ros_control",
                 executable="listener",
                 name="lidar_listener",
             ),
             Node(
                 package="v4l2_camera",
-                namespace="ros_control",
                 executable="v4l2_camera_node",
                 name="flir_driver",
-                parameters=["src/v4l2_camera/v4l2_camera.yaml"],
+                parameters=[
+                    {
+                        "output_encoding": "mono16",
+                        "pixel_format": "Y16 ",
+                        "video_device": "/dev/video4",
+                    }
+                ],
             ),
             Node(
                 package="python_flir_subscriber",
-                namespace="ros_control",
                 executable="listener",
                 name="flir_listener",
             ),
