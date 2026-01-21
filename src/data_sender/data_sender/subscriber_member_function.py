d07defe98abd6dc879a4889ab1976471215bd91d
Jere Lotvonen <jere.lotvonen@gmail.com>
2026-01-21 15:36:45 +0200

    add parameter handling, lidar point publishing, etc...

--- src/data_sender/data_sender/subscriber_member_function.py
+++ src/data_sender/data_sender/subscriber_member_function.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 import rclpy
+from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
 
 from sensor_msgs.msg import PointCloud2
@@ -20,76 +22,107 @@ from sensor_msgs.msg import PointCloud2
 import paho.mqtt.client as mqtt
 
 import json
+import zlib
 
 
 class DataSenderNode(Node):
 
     def __init__(self):
         super().__init__("data_sender")
-        self.get_logger().info("initializing data sender...")
+        self.get_logger().info("Initializing DataSenderNode...")
+
+        # Declare parameters
+        self.declare_parameter("mqtt_host", "localhost")
+
+        # Create callbackgroup
+        self.subscription_callbackgroup = ReentrantCallbackGroup()
 
         # Create Pointcloud2 subscription handler
         self.create_subscription(
             msg_type=PointCloud2,
             topic="/lidar_points",
-            callback=self.pointcloud2_sender_callback,
+            callback=self.send_pointcloud2,
             qos_profile=1,
+            callback_group=self.subscription_callbackgroup,
         )
 
-        # Create mqtt client connection
+        # Create mqtt client
         self.mqtt_client = mqtt.Client()
-        self.mqtt_client.connect("86.50.228.229")
+        # Connect to broker
+        self.mqtt_client.connect(
+            self.get_parameter("mqtt_host").get_parameter_value().string_value
+        )
+        # Start network loop
         self.mqtt_client.loop_start()
 
-        self.get_logger().info("data sender initialized.")
+        self.get_logger().info("DataSenderNode initialized.")
 
-    # Pointcloud2 callback
-    def pointcloud2_sender_callback(self, msg: PointCloud2):
-        header = {
-            "stamp": {
-                "sec": msg.header.stamp.sec,
-                "nanosec": msg.header.stamp.nanosec,
-            },
-            "frame_id": msg.header.frame_id,
-        }
-
-        fields = [
-            {
-                "name": f.name,
-                "offset": f.offset,
-                "datatype": f.datatype,
-                "count": f.count,
+    def send_pointcloud2(self, msg: PointCloud2):
+        try:
+            header = {
+                "stamp": {
+                    "sec": msg.header.stamp.sec,
+                    "nanosec": msg.header.stamp.nanosec,
+                },
             }
-            for f in msg.fields
-        ]
 
-        # TODO: optimize
-        payload = {
-            "header": header,
-            "height": msg.height,
-            "width": msg.width,
-            "fields": fields,
-            "is_bigendian": msg.is_bigendian,
-            "point_step": msg.point_step,
-            "row_step": msg.row_step,
-            "is_dense": msg.is_dense,
-            "data": [int(x) for x in msg.data],
-        }
+            fields = [
+                {
+                    "name": f.name,
+                    "offset": f.offset,
+                    "datatype": f.datatype,
+                    "count": f.count,
+                }
+                for f in msg.fields
+            ]
 
-        self.mqtt_client.publish("test/lidar", json.dumps(payload))
+            metadata = {
+                "header": header,
+                "height": msg.height,
+                "width": msg.width,
+                "fields": fields,
+                "is_bigendian": msg.is_bigendian,
+                "point_step": msg.point_step,
+                "row_step": msg.row_step,
+                "is_dense": msg.is_dense,
+            }
+
+            # Compress with zlib
+            compressed_data = zlib.compress(msg.data)
+
+            # Send the data to MQTT
+            self.get_logger().info("Publishing data to MQTT...")
+            self.mqtt_client.publish(
+                "pointcloud2/metadata",
+                json.dumps(metadata),
+                qos=0,
+            )
+            self.mqtt_client.publish(
+                "pointcloud2/data",
+                compressed_data,
+                qos=0,
+            )
+        except Exception as e:
+            self.get_logger().info(f"MQTT publish failed: {e}")
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    data_sender = DataSenderNode()
+    node = DataSenderNode()
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
 
-    rclpy.spin(data_sender)
+    try:
+        node.get_logger().info("Starting DataSenderNode...")
+        executor.spin()
+    except KeyboardInterrupt:
+        return
 
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
-    data_sender.destroy_node()
+    node.destroy_node()
     rclpy.shutdown()
 
 
