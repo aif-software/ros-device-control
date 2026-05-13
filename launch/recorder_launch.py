d0c1c3ca8b2af6f45727c566be71df648a6b5870
Jere Lotvonen <jere.lotvonen@gmail.com>
2026-05-13 15:16:05 +0300

    update recorder launch

--- launch/recorder_launch.py
+++ launch/recorder_launch.py
@@ -1,22 +1,26 @@
 from launch_ros.actions import Node
 from launch import LaunchDescription
+import os
 import datetime
+
 
 def generate_launch_description():
 
     timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-    return LaunchDescription([
-        Node(
-            package='rosbag2_transport',
-            executable='recorder',
-            name='recorder',
-            output="screen",
-            parameters=[
-                'recorder_params.yaml',
-                {
-                    "storage.uri": f"bags/bag_{timestamp}"
-                }
-            ],
-        )
-    ])
+    params_file = os.getenv("RECORDER_QOS_FILE", default="recorder_params.yaml")
+
+    return LaunchDescription(
+        [
+            Node(
+                package="rosbag2_transport",
+                executable="recorder",
+                name="recorder",
+                output="screen",
+                parameters=[
+                    params_file,
+                    {"storage.uri": f"bags/bag_{timestamp}"},
+                ],
+            )
+        ]
+    )
